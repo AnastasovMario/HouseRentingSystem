fbf1864afc6e6483e1d94e6eae3407a0622cb52c
AnastasovMario <marioanastasov5@gmail.com>
2023-03-25 17:37:28 +0200

    Become Agent implementation

--- HouseRentingSystem/Controllers/AgentController.cs
+++ HouseRentingSystem/Controllers/AgentController.cs
@@ -1,4 +1,6 @@
-﻿using HouseRentingSystem.Core.Models.Agent;
+﻿using HouseRentingSystem.Core.Contracts;
+using HouseRentingSystem.Core.Models.Agent;
+using HouseRentingSystem.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,12 +9,25 @@ namespace HouseRentingSystem.Controllers
     [Authorize]
     public class AgentController : Controller
     {
-        [HttpGet]
-        public IActionResult Become()
+        private readonly IAgentService agentService;
+        public AgentController(IAgentService _agentService)
         {
+            agentService = _agentService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Become()
+        {
+            //we are getting the Users Id from the method we wrote - User.Id
+            //Then from the service we check if he is already an agent;
+            if (await agentService.ExistsById(User.Id()))
+            {
+                return BadRequest();
+            }
+
             return View();
         }
 
+        [HttpPost]
         public async Task<IActionResult> Become(BecomeAgentModel model)
         {
             //връща към първата страница
---
+++ HouseRentingSystem/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace HouseRentingSystem.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        //we can now go into the claims and return the name identifier
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
